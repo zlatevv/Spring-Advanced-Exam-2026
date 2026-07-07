c75d4f6bb1a3d6869d1c03c8d4edd73e7f576450
zlatevv <cvetanzlatev30@gmail.com>
2026-07-07 14:03:24 +0300

    feat: initialize backend structure

---
+++ digitization-service/src/main/java/bg/springadvancedexam/digitizationservice/DigitizationServiceApplication.java
@@ -0,0 +1,13 @@
+package bg.springadvancedexam.digitizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitizationServiceApplication.class, args);
+    }
+
+}
---
+++ digitization-service/src/test/java/bg/springadvancedexam/digitizationservice/DigitizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.springadvancedexam.digitizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
