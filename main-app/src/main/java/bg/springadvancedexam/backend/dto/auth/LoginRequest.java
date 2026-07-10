08f7cbbe21730cfae71f7e1f418d2ba547b24f69
zlatevv <cvetanzlatev30@gmail.com>
2026-07-10 10:15:24 +0300

    feat: custom valid password annotation using passay

--- main-app/src/main/java/bg/springadvancedexam/backend/dto/auth/LoginRequest.java
+++ main-app/src/main/java/bg/springadvancedexam/backend/dto/auth/LoginRequest.java
@@ -1,15 +1,14 @@
 package bg.springadvancedexam.backend.dto.auth;
 
+import bg.springadvancedexam.backend.validation.ValidPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
         @NotBlank(message = "Email is required")
         @Email(message = "Email is not valid")
         String email,
 
-        @NotBlank(message = "Password is required")
-        @Size(min = 5, max = 20, message = "Password must be between {min} and {max} characters")
+        @ValidPassword
         String password) {
 }
--- main-app/src/main/java/bg/springadvancedexam/backend/dto/auth/RegisterRequest.java
+++ main-app/src/main/java/bg/springadvancedexam/backend/dto/auth/RegisterRequest.java
@@ -1,9 +1,9 @@
 package bg.springadvancedexam.backend.dto.auth;
 
+import bg.springadvancedexam.backend.validation.ValidPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotNull
@@ -13,7 +13,6 @@ public record RegisterRequest(
         @Email(message = "Email is not valid")
         String email,
 
-        @NotBlank(message = "Password is required")
-        @Size(min = 5, max = 20, message = "Password must be between {min} and {max} characters")
+        @ValidPassword
         String password) {
 }
