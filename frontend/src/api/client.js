df748f80e99022f9b4175b9e3768e0a3ec930d0a
zlatevv <cvetanzlatev30@gmail.com>
2026-07-23 08:11:32 +0300

    deployment attempt 2

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: '/api'
+    baseURL: import.meta.env.VITE_API_URL || '/api'
 });
 
 apiClient.interceptors.request.use((config) => {
--- main-app/src/main/java/bg/springadvancedexam/mainapp/context/SecurityConfig.java
+++ main-app/src/main/java/bg/springadvancedexam/mainapp/context/SecurityConfig.java
@@ -109,7 +109,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://raramanuscripts.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
